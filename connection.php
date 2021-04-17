b02f4a5b7abe3f827482d317ce237f970c1801d9
SpectoRMoonBlaze <81097576+SpectoRMoonBlaze@users.noreply.github.com>
2021-04-17 21:03:09 +0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'mysql.joinserver.ru:3306'; // адрес сервера 
+$database = 's30241_moon'; // имя базы данных
+$user = 'u30241_L5YqX17Rlt'; // имя пользователя
+$password = 'JmCidq8nAL2msLttaxQdafsN'; // пароль
+?>
